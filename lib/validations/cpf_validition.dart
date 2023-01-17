223eb2a4a8291bde87f73530a176618c822b4879
David Felix <davidffelix@hotmail.com>
2023-01-16 22:37:17 -0300

    Update validation cpf

--- lib/validations/cpf_validition.dart
+++ lib/validations/cpf_validition.dart
@@ -2,18 +2,60 @@ class CPFValidation {
   // soma = 5*10;
   // 5 * 10
   // 529.982.247-25 - Valor de entrada
+
+  primeiroDigitoVerificador('52998224725');
   bool primeiroDigitoVerificador(String cpf) {
     // Pegar o 1° dígito e multiplicar por 10
 
-    // int firstDigit = int.parse(cpf[0]);
-    // int result = firstDigit * 10;
+    int firstDigit = int.parse(cpf[0]);
+    int resultFirst = firstDigit * 10;
+    print(resultFirst);
 
-    // int secondDigit = int.parse(cpf[1]);
-    // int result2 = secondDigit * 9;
+    int secondDigit = int.parse(cpf[1]);
+    int resultSecond = secondDigit * 9;
+    print(resultSecond);
 
-    for (int i = 0; i < cpf.length; i++) {
-      print(cpf[i]);
-    }
+    int thirdDigit = int.parse(cpf[2]);
+    int resultThird = thirdDigit * 8;
+    print(resultThird);
+
+    int fourthDigit = int.parse(cpf[3]);
+    int resultFourth = fourthDigit * 7;
+    print(resultFourth);
+
+    int fifthDigit = int.parse(cpf[4]);
+    int resultFifth = fifthDigit * 6;
+    print(resultFifth);
+
+    int sixthDigit = int.parse(cpf[5]);
+    int resultSixth = sixthDigit * 5;
+    print(resultSixth);
+
+    int seventhDigit = int.parse(cpf[6]);
+    int resultSeventh = seventhDigit * 4;
+    print(resultSeventh);
+
+    int eighthDigit = int.parse(cpf[7]);
+    int resultEighth = eighthDigit * 3;
+    print(resultEighth);
+
+    int ninthDigit = int.parse(cpf[8]);
+    int resultNinth = ninthDigit * 2;
+    print(resultNinth);
+
+    int totalResult = (resultFirst + resultSecond + resultThird + resultFourth + resultFifth + resultSixth + resultSeventh + resultEighth + resultNinth); 
+    print('Resultado da multiplicação dos números comuns: $totalResult.');
+
+    int multiResult = totalResult * 10;
+    print('Resultado do passo 1: $multiResult.');
+
+    
+
+
+
+    // for (int i = 0; i < cpf.length; i++) {
+    //   print(cpf[i]);
+    // }
     return true;
   }
 }
