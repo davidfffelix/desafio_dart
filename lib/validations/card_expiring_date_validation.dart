f45e93b0a32dba26be544b83c7429394247f35ef
David Felix <davidffelix@hotmail.com>
2023-01-19 10:09:17 -0300

    Insert new card_expiring_date

--- lib/validations/card_expiring_date_validation.dart
+++ lib/validations/card_expiring_date_validation.dart
@@ -1,5 +1,5 @@
 class CardExpiringDate {
-  static bool cardExpiringDate({required String card}) {
+  static bool validation({required String card}) {
     if (!RegExp(r'^([0-9]{2})[/-]([0-9]{2})$').hasMatch(card)) {
       print('Your card expiration date is incorrect. Enter the month and year.');
       return false;
--- test/validations/card_expiring_date_validation_test.dart
+++ test/validations/card_expiring_date_validation_test.dart
@@ -1,5 +1,48 @@
+import 'package:desafio_dart/validations/card_expiring_date_validation.dart';
 import 'package:test/test.dart';
 
 void main() {
   // Caminho Errado
+
+  test('the Card Expiring Date number must return true or empty', () {
+    final expirationDate = CardExpiringDate.validation(card: '');
+    expect(expirationDate, false);
+  });
+
+  test('the visa card must return wrong because it was filled out with fewer numbers in the month and year', () {
+    final expirationDate = CardExpiringDate.validation(card: '1-2');
+    expect(expirationDate, false);
+  });
+
+  test('the Card Expiring Date must return incorrectly because a space was inserted between the month and year', () {
+    final expirationDate = CardExpiringDate.validation(card: '01 23');
+    expect(expirationDate, false);
+  });
+
+  test('the Card Expiring Date must return wrong because it was filled with too many digits in the part of the year', () {
+    final expirationDate = CardExpiringDate.validation(card: '02-2023');
+    expect(expirationDate, false);
+  });
+
+  test('the Card Expiring Date must return wrong because it was filled in with the wrong month and with extra numbers in the part of the year', () {
+    final expirationDate = CardExpiringDate.validation(card: '22-2023');
+    expect(expirationDate, false);
+  });
+
+  test('the Card Expiring Date must return wrong because it was filled in with the wrong month and with too many digits in the year', () {
+    final expirationDate = CardExpiringDate.validation(card: '0012-2023');
+    expect(expirationDate, false);
+  });
+
+  // Caminho Correto
+
+  test('the card expiry date should return correctly because it was filled in correctly', () {
+    final expirationDate = CardExpiringDate.validation(card: '01/23');
+    expect(expirationDate, false);
+  });
+
+  test('the expiry date of the card should return correctly because it was correctly filled in with a hyphen', () {
+    final expirationDate = CardExpiringDate.validation(card: '11-20');
+    expect(expirationDate, false);
+  });
 }
